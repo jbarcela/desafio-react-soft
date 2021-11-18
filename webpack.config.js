53fa3d423f602d8fd5447aac237cb1f46f7806c3
Jonathan Barcela <jonathan.barcela@cwi.com.br>
2021-11-17 23:34:50 -0300

    Configuração inicial do projeto - Fast refresh e Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
     mode: isDev ? 'development' : 'production',
     devtool: isDev ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
     devServer: {
-        static: path.resolve(__dirname, 'public')
+        static: path.resolve(__dirname, 'public'),
+        hot: true
     },
     plugins: [
+        isDev && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.[jt]sx?$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+                        },
+                    },
+                ],
             }
         ]
     }
